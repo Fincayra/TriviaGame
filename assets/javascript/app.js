c548fedbb38d2fc4b330ab97acf03940a014b20a
Jay Adams <madam.monarch@yahoo.com>
2020-01-09 18:17:39 -0700

    Ugly ugly ugly. Still trying to work out the jquery/javascript for the game, but for now I've added a background and a very basic div to determine the layout of the page. I'm going to go with a Lord of the Rings theme.

---
+++ assets/javascript/app.js
@@ -0,0 +1,32 @@
+// Trivia game properties
+var correctAnswer = 0;
+var incorrectAnswer = 0;
+var unansweredQuestion = 0;
+var triviaTimer = 20;
+
+// Trivia questions array
+var triviaQuestions = [{
+    question: "blah blah blah",
+    answer: ["blah", "blah", "blah"],
+    correctAnswer: 0 // or whatever number in the index  
+}, {
+    question: "blah blah blah",
+    answer: ["blah", "blah", "blah"],
+    correctAnswer: 0 // or whatever number in the index
+}];
+
+$(document).ready(function () {
+
+// Method to load the game
+window.onload = function () {
+    $("buttonidhere").hide();
+    correctAnswer = 0;
+    incorrectAnswer = 0;
+    unansweredQuestion = 0;
+}
+
+
+
+
+
+});
