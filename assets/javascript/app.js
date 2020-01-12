751b9b8d72d5aab6d1cd41476c74c3e5d03def01
Jay Adams <madam.monarch@yahoo.com>
2020-01-12 15:01:20 -0700

    Slowly getting there. Separated the main menu, quiz, and score summary pages. Need to record answers and wire up the submit button in order to move on to the next question.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,17 +3,21 @@
 
     // trivia q&a's
     var questions = [{
-        question: "which hobbit is my favorite?",
-        answer: ["sam", "pippin", "frodo", "merry"],
-        correctAnswer: 0
+        title: "which hobbit is my favorite?",
+        answers: ['sam', 'pippin', 'frodo', 'merry'],
+        correct: 0
     }, {
-        question: "who is the ranger in the party?",
-        answer: ["frodo", "gimli", "legolas", "aragorn"],
-        correctAnswer: 3
+        title: "who is the ranger in the party?",
+        answers: ['frodo', 'gimli', 'legolas', 'aragorn'],
+        correct: 3
     }, {
-        question: "who is arwen's father?",
-        answer: ["barithor", "legolas", "elrond", "sarumon"],
-        correctAnswer: 2
+        title: "who is arwen's father?",
+        answers: ['barithor', 'legolas', 'elrond', 'sauron'],
+        correct: 2
+    }, {
+        title: "who is the white wizard?",
+        answers: ['gimli', 'sarumon', 'isildor', 'gandolf'],
+        correct: 3
     }];
 
     // countdown clock
@@ -27,33 +31,52 @@
     // starts game
     $(document).ready(function () {
 
+    // hides the quiz on start-up
+    $(".quizbox").hide();
+
+    // hides the summary page
+    $(".summary").hide();
+
     // hides the welcome menu and shows the quiz
     $(".startbox").click(function(a) {
         a.preventDefault();
         $(".startbox").hide();
-        $(".quizbox"). show();
+        $(".quizbox").show();
         showQuestion();
     });
 
+    // // turns the button blue on hover
+    //     $(".quiz ul").on("click", "li", function(){
+    //     $(this).find("button").addClass("clicked");
+    // });
+
+    // // Selecting an answer
+    // $(".quiz a").click(function(e){
+    //     e.preventDefault();
+    //     if($("btn.selected").length){
+    //         let guess = $("btn.selected").attr("[i]");
+    //         console.log(guess);
+    //         } else {
+    //         alert("please select an answer");
+    //     }
+    // });
+
 });
 
-function startQuiz() {
+    // shows the first trivia question / creates radio buttons
+    function showQuestion() {
+    let question = questions[currentQuestion];
+    $(".quiz h2").text(question.title);
+    $(".quiz ul").html("");
+    for(var i = 0; i < question.answers.length; i ++){
+        $(".quiz ul").append("<input type='radio' name='btn' id='" + i + "'>" + question.answers[i] + "</input>");
+    };
+}
+
+    function checkAnswer() {
 
 }
 
-// function showQuestion() {
-//     let question = questions[currentQuestion];
-//     $(".quiz h2").text(questions.title);
-//     $(".quiz ul").html("");
-//     for(var i = 0; i < questions.answer.length; i ++){
-//         $(".quiz ul").append("buttonhere)
-
-// }
-
-function checkAnswer() {
-
-}
-
-function showSummary() {
+    function showSummary() {
 
 }
