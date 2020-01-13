a64c409291bfd97e4b1f1ef6cddb1a0d4de9abfc
Jay Adams <madam.monarch@yahoo.com>
2020-01-13 16:49:24 -0700

    Almost done! Wired up the countdown timer so that it runs only when questions are being answered and when the time runs out, the player is taken to a new page and they have to start the game over again. Still connecting all the pages; can't get the page that allows the user to play the game again to load yet. Next time maybe. Also cleaned up my folder as I realized I had some example files I needed to reference saved there. The page itself is still butt ugly, but as I've learned (the hard way), getting everything to function correctly should've been the first thing that I worked on.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,80 @@
-    var score = 0;
-    var currentQuestion = 0;
+var score = 0;
+var currentQuestion = 0;
 
-    // trivia q&a's
-    var questions = [{
-        title: "which hobbit is my favorite?",
-        answers: ['sam', 'pippin', 'frodo', 'merry'],
-        correct: 0
-    }, {
-        title: "who is the ranger in the party?",
-        answers: ['frodo', 'gimli', 'legolas', 'aragorn'],
-        correct: 3
-    }, {
-        title: "who is arwen's father?",
-        answers: ['barithor', 'legolas', 'elrond', 'sauron'],
-        correct: 2
-    }, {
-        title: "who is the white wizard?",
-        answers: ['gimli', 'sarumon', 'isildor', 'gandolf'],
-        correct: 3
-    }];
+// trivia q&a's
+var questions = [{
+    title: "which hobbit is my favorite?",
+    answers: ['sam', 'pippin', 'frodo', 'merry'],
+    correct: 0
+}, {
+    title: "who is the ranger in the party?",
+    answers: ['frodo', 'gimli', 'legolas', 'aragorn'],
+    correct: 3
+}, {
+    title: "who is arwen's father?",
+    answers: ['barithor', 'legolas', 'elrond', 'sauron'],
+    correct: 2
+}, {
+    title: "who is the white wizard?",
+    answers: ['gimli', 'sarumon', 'isildor', 'gandolf'],
+    correct: 3
+}];
 
-    // countdown clock
-    var clockTimer = 30;
-    var startTime = setInterval(function () {
-        clockTimer--;
-        document.getElementById("countdown").textContent = clockTimer; if (clockTimer <= 0)
-            clearInterval(startTimer);
-    }, 1000);
+// starts game
+$(document).ready(function () {
 
-    // starts game
-    $(document).ready(function () {
-
-    // hides the quiz on start-up
+// hides the quiz on start-up
     $(".quizbox").hide();
 
-    // hides the summary page
+// hides the summary page
     $(".summary").hide();
 
-    // hides the welcome menu and shows the quiz
-    $(".startbox").click(function(a) {
+// hides the fail page
+    $(".fail").hide();
+
+// hides the welcome menu and shows the quiz
+    $(".startbox").click(function (a) {
         a.preventDefault();
         $(".startbox").hide();
         $(".quizbox").show();
         showQuestion();
     });
 
-    // highlights answer
-    $(".quiz ul").on("click", "li", function(){
+// countdown clock
+    var clockTimer = 30;
+    var startTime = setInterval(function () {
+        clockTimer--;
+        document.getElementById("countdown").textContent = clockTimer; if (clockTimer < -1)
+            clearInterval(startTime);
+// if clock runs out and no answer is selected
+        else if (clockTimer === -1) {
+            clearInterval(startTime);
+            $(".quiz").hide();
+            $(".fail").show();
+            $("#countdown").remove();
+        }
+    }, 1000);
+
+// return to start page if no answer is selected
+    $(".fail a").click(function(e){
+        e.preventDefault();
+        $(".startbox").show();
+        $(".quiz").show();
+        $(".fail").hide();
+    })
+
+// "button" customization
+    $(".quiz ul").on("click", "li", function () {
         $(".selected").removeClass("selected");
         $(this).addClass("selected");
     });
-    
-    // tells player to select an answer on submit
-    $(".quiz a").click(function(e){
+
+// tells player to select an answer on submit
+    $(".quiz a").click(function (e) {
         e.preventDefault();
-        if($("li.selected").length){
+        if ($("li.selected").length) {
             let guess = $("li.selected").attr("id");
-            // console.log(guess);
+// console.log(guess);
             checkAnswer(guess);
         } else {
             alert("Please select an answer.");
@@ -65,34 +83,54 @@
 
 });
 
-    // shows the first trivia question / creates radio buttons
-    function showQuestion() {
+// shows the first trivia question / creates radio buttons
+function showQuestion() {
     let question = questions[currentQuestion];
     $(".quiz h2").text(question.title);
     $(".quiz ul").html("");
-    for(var i = 0; i < question.answers.length; i ++){
-        $(".quiz ul").append("<li id='"+i+"'>"+ question.answers[i] + "</li>");
+    for (var i = 0; i < question.answers.length; i++) {
+        $(".quiz ul").append("<li id='" + i + "'>" + question.answers[i] + "</li>");
     };
 }
 
-    // updates the score
-    function checkAnswer(guess) {
-        let question = questions[currentQuestion];
-        if(question.correct == guess){
-            score++;
-        }
-        currentQuestion++;
-        if(currentQuestion >= questions.length){
-            showSummary();
-        } else {
-            showQuestion();
-        }
-
+// updates the score
+function checkAnswer(guess) {
+    let question = questions[currentQuestion];
+    if (question.correct == guess) {
+        score++;
+    }
+    currentQuestion++;
+    if (currentQuestion >= questions.length) {
+        showSummary();
+    } else {
+        showQuestion();
+    }
 }
-    // hides the quiz and shows the summary page
-    function showSummary() {
-        $(".quiz").hide();
-        $(".summary").show();
-    // shows the total score
-        $(".summary p").text("You scored " +score+ " out of " +questions.length+ "!");
+
+// hides the quiz and shows the summary page
+function showSummary() {
+    $(".fail").remove();
+    $("#countdown").hide();
+    $(".quiz").hide();
+    $(".summary").show();
+// shows the total score
+    $(".summary p").text("You scored " + score + " out of " + questions.length + "!");
+    
+// returns player back to the main page
+    $(".summary a").click(function(e) {
+        $(".summary").hide();
+        $(".startbox").show();
+    });
+}
+
+// restarts game??
+function playAgain() {
+    $(".startbox a").click(function(e){
+        a.preventDefault();
+        $(".startbox").hide();
+        $(".quizbox").show();
+        showQuestion();
+
+        score = 0;
+    });
 }
